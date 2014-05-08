47f834f0349ee1ecd1a3922ac8282cd6008869cf
jojofall <Joe@Johanness-MacBook-Air.local>
2014-05-08 20:39:53 +0200

    matrix Multiplikation
    Multiplikation ist dabei, schleife auch, aus ieinem grund will die sau
    nicht wirklich - wenn du zeit hast noch drüberzugehen dann mach das,
    ansonsten erwartete ich dich morgen um 9 mit frühstück oki?
    x

--- givens.cpp
+++ givens.cpp
@@ -1,4 +1,8 @@
-//GIVENS
+////////////////////////////////////////////////
+///////// GIVENS Rotation berechnen ////////////
+////////////////////////////////////////////////
+//////////////////i = 4 quits///////////////////
+
 
 #include <iostream>
 #include <cmath>
@@ -6,44 +10,122 @@
 using namespace std;
 
 int main(){
-    double M[3][3],MatrixBegin[3][3],cosine,sine;
+    double M[3][3],MatrixBegin[3][3],MatrixBeginTemp[3][3],cosine,sine;
     int i,j,ii,jj;
     
-    cout<<"geben sie i ein"<<endl;
-    cin>>ii;
-    cout<<"geben sie j ein"<<endl;
-    cin>>jj;
+    //  Anfangscredits
+    
+    cout << endl << "////////////////////////////////////////////////" << endl << "///////// GIVENS Rotation berechnen ////////////" << endl << "////////////////////////////////////////////////"<< endl<<"//////////////////i = 4 quits///////////////////" << endl;
+
+    
+    //  Anfangsmatrix befüllen
     
     for(int i=1;i<=3;i++){
         for(int j=1;j<=3;j++){
             MatrixBegin[i-1][j-1]=j*i;
+            MatrixBeginTemp[i-1][j-1]=j*i;
         }
     }
- 
-    for(int i=0;i<=2;i++){
-        for(int j=0;j<=2;j++){
-            if(i==j){
-                M[i][j]=1.;
-            }
-            else{
-                M[i][j]=0.;
-            }
-        }
+    
+    
+    //HIER MUSS SCHLEIFE BEGINNEN
+    
+    while(i!=4){
+    
+        //  i und j einlesen
+    
+        cout<<"i: ";
+        cin>>ii;
+        if (ii==4){return 88;}
+        else{
+        cout<<endl<<"j: ";
+        cin>>jj;
         
-    }
+        cosine=MatrixBegin[jj-1][jj-1]/sqrt(pow(MatrixBegin[jj-1][jj-1],2)+pow(MatrixBegin[ii-1][jj-1],2));
+        sine=-MatrixBegin[ii-1][jj-1]/sqrt(pow(MatrixBegin[jj-1][jj-1],2)+pow(MatrixBegin[ii-1][jj-1],2));
 
-    cosine=M[jj][jj]/sqrt(pow(M[jj][jj],2)+pow(M[ii][jj],2));
-    sine=-M[ii][jj]/sqrt(pow(M[jj][jj],2)+pow(M[ii][jj],2));
+        //  GivensMatrix befüllen
+        
+        for(int i=0;i<=2;i++){
+            for(int j=0;j<=2;j++){
+                if (i==j && (i==ii-1 || j==jj-1)){
+                    M[i][j]=cosine;
+                }
+                else if (i==j && (i!=ii-1 || j!=jj-1)){
+                    M[i][j]=1;
+                }
+                else if (i==ii-1 && j==jj-1){
+                    M[i][j]=sine;
+                }
+                else if (j==ii-1 && i==jj-1){
+                    M[i][j]=-sine;
+                }
+                else{
+                    M[i][j]=0.;
+                }
+            }
+        
+        }
+   
+        cout << "i= " << ii << ",    " << "j= " << jj << endl;
     
+        //  Anfangsmatrix ausgeben
     
+        cout << "Anfangsmatrix: " << endl;
     
-    return 0;
-}
-
-void dot(double matrix1[3][3],double matrix2[3][3],double matrix3[3][3]){
-    for(int g=0;g<=2;g++){
-        for(int h=0;h<=2;g++){
-            matrix3[g][h]=matrix1[g][h]*matrix2[h][g]+matrix3[g][h];
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                cout << "  " << MatrixBegin[i][j]  <<  " ";
+            }
+            cout  << endl;
+        }
+    
+        //Givens matrix ausgeben
+        
+        cout << "Givensmatrix: " << endl;
+        
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                cout << "  " << M[i][j]  <<  " ";
+            }
+        
+            cout  << endl;
+        
+            if (i==2){
+                cout << endl;}
+        }
+    
+        // Schleife 1 Test:
+    
+        double matrixSchleife1[i][j];
+    
+        for(i=0; i<3; i++){
+            for(j=0; j<3; j++){
+                matrixSchleife1[i][j] = 0;
+                for(int k=0; k<3; k++){
+                    matrixSchleife1[i][j] += M[i][k] * MatrixBegin[k][j];
+                }
+                MatrixBegin[i][j]=matrixSchleife1[i][j];
+            }
+        }
+    
+        //1. Multiplizierte matrix ausgeben
+    
+        cout << "1. Multiplizierte matrix: " << endl;
+    
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                cout << "  " << matrixSchleife1[i][j]  <<  " ";
+            }
+        
+            cout  << endl;
+        
+            if (i==2){
+                cout << endl;
+            }
+        }
         }
     }
+
+
 }
